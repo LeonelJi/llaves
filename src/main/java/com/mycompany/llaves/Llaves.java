d5a7e50d0d6c12738a4cf5cf7b6a663c8ec784a1
LeonelJi <lj924618@gmail.com>
2024-11-23 12:23:45 -0500

    commit1

---
+++ src/main/java/com/mycompany/llaves/Llaves.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.llaves;
+
+/**
+ *
+ * @author User
+ */
+public class Llaves {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
